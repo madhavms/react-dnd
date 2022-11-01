375ebfe3f3e79898aab70c82f69da119d9b4b428
Madhav Manohar S <madhavmanohar.s@gmail.com>
2022-11-01 20:56:52 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import DragNDrop from "./components/DragNDrop";
+import "./App.css";
+
+const data = [
+  { title: "Group 1", items: ["1", "2", "3"] },
+  { title: "Group 2", items: ["4", "5"] },
+];
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <DragNDrop data={data}/>
       </header>
     </div>
   );
